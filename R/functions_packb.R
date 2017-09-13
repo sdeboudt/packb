20ede773a8ac0fa0d41e59f284ca8fb6bf666e98
Stephane <sdeboudt@hotmail.com>
2017-09-13 16:55:00 +0200

    Initial Commit

---
+++ R/functions_packb.R
@@ -0,0 +1,13 @@
+
+#' @title f1 exposed by packb
+#' @description This is function \code{f1} exposed by \code{packb}
+#'
+#' @return A String "Hello from packb_f1"
+#' @export
+#'
+#' @examples
+#' packb_f1()
+packb_f1 <- function() {
+  print("Hello from packb_f1")
+}
+
